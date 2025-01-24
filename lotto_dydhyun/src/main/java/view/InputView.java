d0f686195f28955009ebfe66e9cfcfb4a209e942
dydhyun <dydgus625@naver.com>
2025-01-24 23:04:33 +0900

    feat: InputView 당첨번호와 보너스번호 입력받기 + 테스트용 생성자와메서드

--- lotto_dydhyun/src/main/java/view/InputView.java
+++ lotto_dydhyun/src/main/java/view/InputView.java
@@ -1,19 +1,46 @@
 package view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputView {
     private static final String ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     private static final int PURCHASE_SCALE = 1000;
 
     public int gameCount = 0;
 
-    public InputView(String userInput) {
-        purchase(userInput);
+    public InputView() {
+        purchase();
 
         System.out.println(gameCount + "개를 구매했습니다.");
     }
 
-    private void purchase(String userInput){
+    InputView(String userInput) {
+        purchaseTest(userInput);
+
+        System.out.println(gameCount + "개를 구매했습니다.");
+    }
+
+    public int getGameCount() {
+        return gameCount;
+    }
+
+    private void purchaseTest(String userInput){
+        int purchaseAmount = validateInputType(userInput);
+
+        validateInputPositiveNumber(purchaseAmount);
+        validateInputScale(purchaseAmount);
+
+        this.gameCount = purchaseAmount / PURCHASE_SCALE;
+    }
+
+    private void purchase(){
         System.out.println(ENTER_PURCHASE_AMOUNT);
+
+        String userInput = Console.readLine();
 
         int purchaseAmount = validateInputType(userInput);
 
@@ -45,5 +72,34 @@ public class InputView {
         if (userInput % 1000 != 0)
             throw new IllegalArgumentException("[ERROR] 입력단위는 " + PURCHASE_SCALE + "원 입니다.");
     }
+
+
+    public Lotto enterWinningNumber(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+
+        List<Integer> winningNumbers = parseInputToList(Console.readLine());
+
+        return new Lotto(winningNumbers);
+    }
+
+    private List<Integer> parseInputToList(String rawInput){
+        List<Integer> winningNumber = new ArrayList<>();
+
+        String[] splitInput = rawInput.split(",");
+
+        for (String num : splitInput) {
+            winningNumber.add(Integer.parseInt(num.trim()));
+        }
+
+        return winningNumber;
+    }
+
+
+    public int enterBonusNumber(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+
+        return Integer.parseInt(Console.readLine());
+    }
+
 
 }
